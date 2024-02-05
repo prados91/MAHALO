b1459a5af170d64a22c4b5aa97c15045c2d847e5
Lucas Barrera <agustinbarrera.ri@gmail.com>
2024-02-05 00:33:30 -0300

    cambios en el icono flotante de wsp

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,28 @@
-import ReactDOM from 'react-dom/client'
-import React from 'react'
-import App from './App.jsx'
-import LogoWhatsapp from '../src/assets/logo-whatsapp.png'
+import ReactDOM from "react-dom/client";
+import React from "react";
+import App from "./App.jsx";
+import { FaWhatsapp } from "react-icons/fa"; // Importa el ícono de WhatsApp desde react-icons
 
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-const phoneNumber = "5491124752671"
+const phoneNumber = "5491124752671";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-        <div className="main-whatsapp">
-            <a href={`https://api.whatsapp.com/send?phone=${phoneNumber}`} target="_blank"><img
-                src={LogoWhatsapp} alt="Logo de WhatsApp" /></a>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+    <div className="main-whatsapp">
+      <a
+        href={`https://api.whatsapp.com/send?phone=${phoneNumber}`}
+        target="_blank"
+        rel="noReferrer"
+        className="whatsapp-container"
+      >
+        <div className="whatsapp-link">
+          <FaWhatsapp size={40} color="#499FB6" />
         </div>
-    </React.StrictMode>,
-)
+      </a>
+    </div>
+  </React.StrictMode>
+);

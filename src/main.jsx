27dd91dc987b854bfce699a821ad6595de6864d3
Augusto <augustoprados91@gmail.com>
2024-02-14 14:26:33 -0300

    AP - Buenas práticas HTML

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,21 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App.jsx";
-import { FaWhatsapp } from "react-icons/fa";
+
 
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-import { phoneNumber } from "./assets/db.js";
 
+/*
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <div>
-            <App />
-            <div className="main-whatsapp">
-                <a
-                    href={`https://api.whatsapp.com/send?phone=${phoneNumber}`}
-                    target="_blank"
-                    rel="noReferrer"
-                    className="whatsapp-container"
-                >
-                    <div className="whatsapp-link">
-                        <FaWhatsapp size={40} color="#F8F9FA" />
-                    </div>
-                </a>
-            </div>
-        </div>
+        <App />
     </React.StrictMode>
+);*/
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <App />
 );
